49a2b69354ba87de226952418daf2827ea4ae7d9
MohamedHassan-toobler <mohamed.hassan.tblr@gmail.com>
2025-07-16 18:16:20 +0530

    Initial Setup

--- tests/testdemo.spec.js
+++
@@ -1,9 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('homepage has title and header', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  await expect(page).toHaveTitle(/Playwright/);
-  const header = await page.locator('text=Playwright');
-  //await expect(header).toBeVisible();
-  //
-});
