bee76940672e0fa59120aaac024777979e6ee191
MohamedHassan-toobler <mohamed.hassan.tblr@gmail.com>
2025-09-10 18:28:58 +0530

    lmApprvoal

--- e2e/Employee Service Requests/Leaves.spec.js
+++ e2e/Employee Service Requests/Leaves.spec.js
@@ -5,7 +5,7 @@ const dataset = JSON.parse(
   JSON.stringify(require("../../utils/loginTestData.json"))
 );
 const { API } = require("../../utils/API");
-test.only("Extract and print id_token", async ({ page, request }) => {
+test("Extract and print id_token", async ({ page, request }) => {
   const loginPage = new LoginPage(page);
   await loginPage.launchingPage(dataset.url);
   await loginPage.login(dataset.username, dataset.password);
@@ -13,5 +13,11 @@ test.only("Extract and print id_token", async ({ page, request }) => {
   const api = new API(page, request);
   console.log(await api.getToken());
   await api.createAnnualLeave();
+});
+test.only("Approve using LM", async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.launchingPage(dataset.url);
+  await loginPage.login(dataset.lmUsername, dataset.lmPassword);
+  await page.getByText("Employee Service Request").click();
   await page.pause();
 });
